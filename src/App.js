b306bb792c55885d7efa67cd99eea466029360a0
Sam Coffey <coffey.b.sam@gmail.com>
2022-01-30 16:12:50 -0800

    Change site title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Game from '@components/Game';
 function App() {
   return (
     <div className="App">
+      <div className="site-title">
+        Not Wordle
+      </div>
       <Game />
     </div>
   );
