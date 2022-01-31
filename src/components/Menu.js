402b4d42bf89c0d300d003e24d5e653cdbe85663
Sam Coffey <coffey.b.sam@gmail.com>
2022-01-30 19:01:43 -0800

    Menu input handling & grid responsiveness for mobile

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,13 +6,19 @@ export default function Menu(props) {
   const [inputWord, setInputWord] = useState('');
 
   const onBegin = (word) => {
-    console.log('onBegin', 'inputWord', word);
-    props.onBegin(word);
+    if (!isInputWordValidWord()) {
+      alert('Not a valid word');
+      return;
+    }
+    if (isInputWordValidSize()) 
+      props.onBegin(word);
   }
 
   const onInputKeyPress = (e) => {
     if (e.keyCode === KeyCodes.ENTER) {
       onBegin(e.target.value);
+    } else if (!Word.isInAlphabet(e.key) && e.keyCode !== KeyCodes.BACKSPACE) {
+      e.preventDefault();
     }
   }
 
@@ -20,18 +26,28 @@ export default function Menu(props) {
     onBegin(Word.generateRandomWord());
   }
 
+  const isInputWordValidSize = () => {
+    return inputWord.length >= 4 && inputWord.length <= 7;
+  }
+
+  const isInputWordValidWord = () => {
+    return Word.isInWordList(inputWord);
+  }
+
   return (
     <>
-      <p className="text">Enter a word to begin</p>
+      <p className="text" style={{ marginBottom: '0' }}>Enter a word to begin</p>
+      <p style={{ color: '#b2bec3', margin: '0.2em' }}>(4 to 7 characters)</p>
       <input 
         type="text"
+        maxLength={7}
         className="wordle-input"
         value={inputWord} 
         onChange={ event => setInputWord(event.target.value) }
         onKeyDown={onInputKeyPress}
       />
       <div className="menu-buttons">
-        <button disabled={inputWord.length < 4} onClick={() => onBegin(inputWord)}>Begin</button>
+        <button disabled={!isInputWordValidSize()} onClick={() => onBegin(inputWord)}>Begin</button>
         <button onClick={() => onClickSurpriseMe()}>Surprise Me</button>
       </div>
     </>
